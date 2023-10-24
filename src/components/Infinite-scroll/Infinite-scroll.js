3547c819952c2529adf1822b581e5d84eeea52b0
Alexey Kazhanov <akazhanov72@yandex.ru>
2023-10-24 16:53:01 +0500

    Infinite Scroll

--- src/components/Infinite-scroll/Infinite-scroll.js
+++ src/components/Infinite-scroll/Infinite-scroll.js
@@ -4,7 +4,14 @@ function InfiniteScroll() {
     return (
         <main>
             <section className='infinite-scroll'>
-                
+                <header className='header'>
+                    <h1 className='header__title'>UPLASH API - INFINITE SCROLL</h1>
+                </header>
+                <div className='images__container'>
+                    <img src='https://images.unsplash.com/photo-1696790857863-e41b641311be?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' className='images__image' alt='' />
+                    <img src='https://images.unsplash.com/photo-1696790857863-e41b641311be?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' className='images__image' alt='' />
+                    <img src='https://images.unsplash.com/photo-1696790857863-e41b641311be?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' className='images__image' alt='' />
+                </div>
             </section>
         </main>
     )
