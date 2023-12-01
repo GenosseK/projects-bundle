8b19a5c86c7f9df16ef29f4cd8961e405797652e
Alexey Kazhanov <akazhanov72@yandex.ru>
2023-12-01 18:31:48 +0500

    Book Keeper

--- src/components/Book-Keeper/Book-Keeper.js
+++ src/components/Book-Keeper/Book-Keeper.js
@@ -1,12 +1,31 @@
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useRef, useState } from 'react';
 import './Book-Keeper.css';
 
 function BookKeeper() {
+
+    const [isPopupOpened, setIsPopupOpened] = useState(false);
+
+    const openPopup = () => {
+        setIsPopupOpened(true)
+    }
+
+    const closePopup = () => {
+        setIsPopupOpened(false)
+    }
+
+    function closeByOverlay(evt) {
+        if (evt.target === evt.currentTarget) {
+            closePopup();
+        }
+      }
+  
+
     return (
         <main className='bookKeeper'>
 
-            <div className='bookKeeper__title_container'>
+            <div className='bookKeeper__title_container' onClick={openPopup}>
                 <h1 className='bookKeeper__title'>Add Bookmark</h1>
             </div>
 
@@ -36,9 +55,10 @@ function BookKeeper() {
 
 
 
-            <div className='bookMark__creator__container' id='modal'>
+            <div  onMouseDown={closeByOverlay} className={`bookMark__creator__container ${isPopupOpened ? 'bookMark__creator__container_open' : ''}`}>
+
                 <div className='creator'>
-                    <FontAwesomeIcon icon={faTimes} className='creator__close-icon' />
+                    <FontAwesomeIcon icon={faTimes} className='creator__close-icon' onClick={closePopup} />
                     <div className='creator__title_container'>
                         <h3 className='creator__title'>Add Bookmark</h3>
                     </div>
