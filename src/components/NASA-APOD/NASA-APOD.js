5f290ae7ac7c1e292c456dc25226d6167d5b867f
Alexey Kazhanov <akazhanov72@yandex.ru>
2023-12-16 16:20:43 +0500

    NASA APOD

--- src/components/NASA-APOD/NASA-APOD.js
+++ src/components/NASA-APOD/NASA-APOD.js
@@ -1,8 +1,35 @@
 import './NASA-APOD.css';
+import loaderRocket from '../../images/rocket.svg';
+import { useState } from 'react';
 
 function NasaApod() {
-    return(
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    return (
         <main className='nasa-apod'>
+
+            {isLoading && (
+                <div className='nasa-apod__loader-container'>
+                    <img src={loaderRocket} alt='Loading' />
+                </div>
+            )}
+
+       
+                <div className='nasa-apod__navigation-container'>
+
+                    <div className='nasa-apod__navigation__main-container'>
+                        <button className='nasa-apod__navigation__button'>Favorites</button>
+                        <span className='nasa-apod__navigation__separator'>•</span>
+                        <button className='nasa-apod__navigation__button'>Load more</button>
+                    </div>
+
+                </div>
+
+                <div className='nasa-apod__container'>
+                    
+                </div>
+           
 
         </main>
     )
