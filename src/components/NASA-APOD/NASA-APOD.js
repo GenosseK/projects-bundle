b1275d1e7b5995146b43e8d772207c3d7fb3746c
Alexey Kazhanov <akazhanov72@yandex.ru>
2023-12-27 16:51:20 +0500

    NASA APOD

--- src/components/NASA-APOD/NASA-APOD.js
+++ src/components/NASA-APOD/NASA-APOD.js
@@ -11,13 +11,25 @@ function NasaApod() {
     const [imagesLoaded, setImagesLoaded] = useState(0);
     const [totalImages, setTotalImages] = useState(0);
     const [favorites, setFavorites] = useState([]);
+    const [saveConfirmed, setSaveConfirmed] = useState(false);
+
 
     const count = 3;
     const apiKey = 'JETYJShcEbqj1MH7lb4cc2Ce6URyfK5sFgZ0FZgB';
     const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
+    const saveFavoritesToLocalStorage = (favorites) => {
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+    };
+
+    const getFavoritesFromLocalStorage = () => {
+        const storedFavorites = localStorage.getItem('favorites');
+        return storedFavorites ? JSON.parse(storedFavorites) : [];
+    };
+
     useEffect(() => {
         fetchImages();
+        setFavorites(getFavoritesFromLocalStorage());
     }, []);
 
     const fetchImages = () => {
@@ -50,6 +62,22 @@ function NasaApod() {
             });
     };
 
+    const fetchNewImages = () => {
+        setIsLoading(true);
+        setApodData([]); // Reset apodData to clear the existing images
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                setApodData(data);
+                setTotalImages(data.length);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching NASA APOD images:', error);
+                setIsLoading(false);
+            });
+    };
+
     const imageLoaded = () => {
         setImagesLoaded(imagesLoaded + 1);
         if (imagesLoaded === totalImages) {
@@ -59,7 +87,7 @@ function NasaApod() {
     };
 
     const handleScroll = () => {
-        if ( nasaApodSection === 'main' && window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        if (nasaApodSection === 'main' && window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
             setReady(false);
             fetchMoreImages();
         }
@@ -74,18 +102,33 @@ function NasaApod() {
     }, [ready, apodData]);
 
     const addToFavorites = (apod) => {
-        setFavorites((prevFavorites) => [...prevFavorites, apod]);
+        const isAlreadyInFavorites = favorites.some((fav) => fav.date === apod.date);
+
+        if (isAlreadyInFavorites) {
+            removeFromFavorites(apod);
+            setSaveConfirmed('Removed!');
+        } else {
+            setFavorites((prevFavorites) => [...prevFavorites, apod]);
+            setSaveConfirmed('Added!');
+        }
+
+        saveFavoritesToLocalStorage(favorites);
+
+        setTimeout(() => {
+            setSaveConfirmed(false);
+        }, 3000);
     };
 
     const removeFromFavorites = (apod) => {
-        setFavorites((prevFavorites) => prevFavorites.filter((fav) => fav.date !== apod.date));
+        const updatedFavorites = favorites.filter((fav) => fav.date !== apod.date);
+        setFavorites(updatedFavorites);
+
+        saveFavoritesToLocalStorage(updatedFavorites);
     };
 
-    // Function to toggle between main and favorites sections
     const toggleSection = (section) => {
         setNasaApodSection(section);
     };
-
 
     return (
         <main className='nasa-apod'>
@@ -104,7 +147,7 @@ function NasaApod() {
                     <div className='nasa-apod__navigation__main-container'>
                         <button className='nasa-apod__navigation__button' onClick={() => toggleSection('favorites')} >Favorites</button>
                         <span className='nasa-apod__navigation__separator'>•</span>
-                        <button className='nasa-apod__navigation__button'>Load more</button>
+                        <button className='nasa-apod__navigation__button' onClick={fetchNewImages}>Load more</button>
                     </div>
 
                 )}
@@ -123,14 +166,18 @@ function NasaApod() {
 
                 {nasaApodSection === 'main' && apodData.map((apod) => (
                     <div onLoad={imageLoaded} key={apod.date} className='nasa-apod__card'>
-                        <img src={apod.url} alt='' className='nasa-apod__card__image'></img>
+                        <a href={apod.hdurl} target="_blank" rel="noopener noreferrer">
+                            <img src={apod.url} alt='' className='nasa-apod__card__image'></img>
+                        </a>
                         <div className='nasa-apod__card__body'>
                             <h5 className='nato-apod__card__title'>{apod.title}</h5>
-                            <p className='nasa-apod__card__favorites-button' onClick={() => addToFavorites(apod)} >Add To Favorites</p>
+                            <p className='nasa-apod__card__favorites-button' onClick={() => addToFavorites(apod)}>
+                                {favorites.some((fav) => fav.date === apod.date) ? 'Remove from Favorites' : 'Add To Favorites'}
+                            </p>
                             <p className='nasa-apod__card__text'>{apod.explanation}</p>
                             <small className='nasa-apod__card__sub-text'>
                                 <strong>{apod.date}</strong>
-                                <span> Copyright Info</span>
+                                <span> {apod.copyright || ''}</span>
                             </small>
                         </div>
                     </div>
@@ -139,7 +186,9 @@ function NasaApod() {
                 {nasaApodSection === 'favorites' &&
                     favorites.map((apod) => (
                         <div key={apod.date} className='nasa-apod__card'>
-                            <img src={apod.url} alt='' className='nasa-apod__card__image'></img>
+                            <a href={apod.hdurl} target="_blank" rel="noopener noreferrer">
+                                <img src={apod.url} alt='' className='nasa-apod__card__image'></img>
+                            </a>
                             <div className='nasa-apod__card__body'>
                                 <h5 className='nato-apod__card__title'>{apod.title}</h5>
                                 <p
@@ -151,7 +200,7 @@ function NasaApod() {
                                 <p className='nasa-apod__card__text'>{apod.explanation}</p>
                                 <small className='nasa-apod__card__sub-text'>
                                     <strong>{apod.date}</strong>
-                                    <span> Copyright Info</span>
+                                    <span> {apod.copyright || ''}</span>
                                 </small>
                             </div>
                         </div>
@@ -159,9 +208,11 @@ function NasaApod() {
 
             </div>
 
-            <div className='nasa-apod__save-confirmed'>
-                <h1>Added!</h1>
-            </div>
+            {saveConfirmed && (
+                <div className='nasa-apod__save-confirmed'>
+                    <h1>{saveConfirmed}</h1>
+                </div>
+            )}
 
 
         </main>
