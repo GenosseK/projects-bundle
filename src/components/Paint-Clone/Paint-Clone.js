4effcebdd2555d0e9f58f3db327fb094ede5652a
Alexey Kazhanov <akazhanov72@yandex.ru>
2024-03-05 18:08:43 +0500

    Paint Clone

--- src/components/Paint-Clone/Paint-Clone.js
+++ src/components/Paint-Clone/Paint-Clone.js
@@ -9,7 +9,11 @@ function PaintClone() {
     const contextRef = useRef(null);
     const [brushSize, setBrushSize] = useState(10);
     const [brushColor, setBrushColor] = useState('#a51dab');
-    const [canvasColor, setCanvasColor] = useState('#FFFFFF')
+    const [canvasColor, setCanvasColor] = useState('#FFFFFF');
+    const [appliedCanvasColor, setAppliedCanvasColor] = useState('#FFFFFF');
+
+    const [currentTool, setCurrentTool] = useState('brush');
+    const [message, setMessage] = useState('');
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -21,7 +25,7 @@ function PaintClone() {
 
         context.fillStyle = 'white';
         context.fillRect(0, 0, canvas.width, canvas.height);
-    
+
         context.lineCap = 'round';
         context.strokeStyle = brushColor;
         context.lineWidth = 10;
@@ -39,13 +43,21 @@ function PaintClone() {
     const draw = ({ nativeEvent }) => {
         if (!isDrawing) return;
         const { offsetX, offsetY } = nativeEvent;
+        if (currentTool === 'eraser') {
+            contextRef.current.strokeStyle = appliedCanvasColor;
+        } else {
+            contextRef.current.strokeStyle = brushColor;
+        }
         contextRef.current.lineTo(offsetX, offsetY);
         contextRef.current.stroke();
     };
-
     const endDrawing = () => {
         contextRef.current.closePath();
         setIsDrawing(false);
+    };
+
+    const toggleTool = (tool) => {
+        setCurrentTool(tool);
     };
 
     const handleBrushColorChange = (e) => {
@@ -68,6 +80,47 @@ function PaintClone() {
         const context = canvas.getContext('2d');
         context.fillStyle = canvasColor;
         context.fillRect(0, 0, canvas.width, canvas.height);
+        setAppliedCanvasColor(canvasColor);
+    };
+
+    const handleSaveCanvasToLocalStorage = () => {
+        const canvas = canvasRef.current;
+        const dataURL = canvas.toDataURL();
+        localStorage.setItem('savedCanvas', dataURL);
+        setMessage('Canvas Saved');
+        setTimeout(() => setMessage(''), 2000);
+    };
+
+    const handleLoadCanvasFromLocalStorage = () => {
+        const savedCanvas = localStorage.getItem('savedCanvas');
+        if (savedCanvas) {
+            const img = new Image();
+            img.onload = () => {
+                contextRef.current.drawImage(img, 0, 0);
+                setMessage('Canvas Loaded');
+                setTimeout(() => setMessage(''), 2000);
+            };
+            img.src = savedCanvas;
+        }
+    };
+
+    const handleSaveCanvas = () => {
+        const canvas = canvasRef.current;
+        const dataURL = canvas.toDataURL('image/jpeg');
+        const link = document.createElement('a');
+        link.href = dataURL;
+        link.download = 'canvas_image.jpg';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        setMessage('Canvas Saved');
+        setTimeout(() => setMessage(''), 2000);
+    };
+
+    const handleClearLocalStorage = () => {
+        localStorage.removeItem('savedCanvas');
+        setMessage('Saved Canvas Cleared');
+        setTimeout(() => setMessage(''), 2000);
     };
 
     return (
@@ -76,11 +129,13 @@ function PaintClone() {
             <div className='PaintClone__top-bar'>
 
                 <div className='PaintClone__active-tool'>
-                    <span className='PaintClone__active-tool_text' title='Active Tool'>Brush</span>
+                <span className='PaintClone__active-tool_text' title='Active Tool'>
+                        {message ? message : (currentTool === 'eraser' ? 'Eraser' : (currentTool === 'brush' ? 'Brush' : ''))}
+                    </span>
                 </div>
 
-                <div className='PaintClone__brush PaintClone__tool'>
-                    <FontAwesomeIcon icon={faBrush} className='PaintClone__icon' />
+                <div className={`PaintClone__brush PaintClone__tool ${currentTool === 'brush' ? 'active' : ''}`} >
+                    <FontAwesomeIcon icon={faBrush} className='PaintClone__icon' onClick={() => toggleTool('brush')} />
                     <input className='PaintClone__jscolor' type='color' defaultValue='#a51dab' onChange={handleBrushColorChange} />
                     <span className='PaintClone__size' title='Brush Size'>{brushSize}</span>
                     <input className='PaintClone__slider' type='range' min='1' max='50' defaultValue='10' onChange={handleSizeChange} />
@@ -91,29 +146,27 @@ function PaintClone() {
                     <input className='PaintClone__jscolor' type='color' value={canvasColor} onChange={handleCanvasColorChange} />
                 </div>
 
-                <div className='PaintClone__tool'>
+                <div className={`PaintClone__tool ${currentTool === 'eraser' ? 'active' : ''}`} onClick={() => toggleTool('eraser')}>
                     <FontAwesomeIcon icon={faEraser} title='Eraser' className='PaintClone__icon' />
                 </div>
 
                 <div className='PaintClone__tool'>
-                    <FontAwesomeIcon icon={faUndoAlt} title='Clear' className='PaintClone__icon PaintClone__undo-alt' />
+                    <FontAwesomeIcon icon={faUndoAlt} title='Step Back' className='PaintClone__icon PaintClone__undo-alt' />
                 </div>
 
                 <div className='PaintClone__tool'>
-                    <FontAwesomeIcon icon={faDownload} title='Save Local Storage' className='PaintClone__icon' />
+                    <FontAwesomeIcon icon={faDownload} title='Save Local Storage' className='PaintClone__icon' onClick={handleSaveCanvasToLocalStorage} />
                 </div>
-
                 <div className='PaintClone__tool'>
-                    <FontAwesomeIcon icon={faUpload} title='Load Local Storage' className='PaintClone__icon' />
+                    <FontAwesomeIcon icon={faUpload} title='Load Local Storage' className='PaintClone__icon' onClick={handleLoadCanvasFromLocalStorage} />
                 </div>
-
                 <div className='PaintClone__tool'>
-                    <FontAwesomeIcon icon={faTrashAlt} title='Clear Local Storage' className='PaintClone__icon PaintClone__trash-alt' />
+                    <FontAwesomeIcon icon={faTrashAlt} title='Clear Local Storage' className='PaintClone__icon PaintClone__trash-alt' onClick={handleClearLocalStorage} />
                 </div>
 
                 <div className='PaintClone__tool'>
                     <a>
-                        <FontAwesomeIcon icon={faSave} title='Save Local Storage' className='PaintClone__icon' />
+                        <FontAwesomeIcon icon={faSave} title='Download Image' className='PaintClone__icon' onClick={handleSaveCanvas} />
                     </a>
                 </div>
 
